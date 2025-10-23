5becdf837d0bce86ba643c87f31139cd7d874ff3
msarah-09 <sarahmacedo0908@gmail.com>
2025-10-23 23:32:23 +0000

    Corrigindo espacos

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,14 +8,14 @@ else:
 
 if last_digit > 5:
     print(
-     f"Last digit of {number} is {last_digit} and is greater than 5"
+         f"Last digit of {number} is {last_digit} and is greater than 5"
     )
 elif last_digit == 0:
     print(
-    f"Last digit of {number} is {last_digit} and is 0"
+        f"Last digit of {number} is {last_digit} and is 0"
      )
 else:
     print(
-    f"Last digit of {number} is {last_digit} "
-    f"and is less than 6 and not 0"
+         f"Last digit of {number} is {last_digit} "
+         f"and is less than 6 and not 0"
     )
